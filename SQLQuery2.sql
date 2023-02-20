9b9a262c0804fc47c7f283244a44cc011919cbbd
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-02-20 04:26:33 -0800

    Q2 task 3+4+5+6

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -17,15 +17,32 @@ VALUES
 SELECT month(sale_date) as SalesMonth, SUM(sale_amount) as TotalSales
 FROM sales 
 GROUP BY  MONTH(sale_date)
-/*TASK 1*/
+/*TASK 1. Write a SQL query that calculates the total sales for each month in the sales table.*/
 /*group the yaer and month beside month sales*/
 SELECT YEAR(sale_date) as SalesYear, month(sale_date) as SalesMonth, SUM(sale_amount) as TotalSales
 FROM sales 
 GROUP BY YEAR(sale_date), MONTH(sale_date)
 ORDER BY YEAR(sale_date), MONTH(sale_date)
 
-/*TASK 2*/
+/*TASK 2. Write a SQL query that calculates the average of sales amount.
+*/
 SELECT AVG(sale_amount)as sales_average
 FROM sales
 
-/*TASK 3*/
+/*TASK 3. Write a SQL query that counts how many products sold availabe in the table.
+*/
+SELECT COUNT(sale_amount) from sales
+
+/*TASK 4. Write a SQL query that shows the minimum and maximum sale amount in the table name it as 'lowest amount' , 'higest amount'.
+*/
+SELECT MIN(sale_amount)as 'lowest amount', MAX(sale_amount) as 'higest amount' FROM sales
+ 
+ /*TASK 5. Write a SQL query that organize the data in decending order using the id.
+*/
+SELECT * FROM sales
+ORDER BY sale_id DESC
+
+/*TASK6. Write a SQL query that shows the sales between 01-01-2022 and 01-02-2022.
+*/
+SELECT * FROM SALES 
+WHERE sale_date BETWEEN '01-01-2022' AND '01-02-2022'
